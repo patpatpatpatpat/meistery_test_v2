2617f1f0c0ca955f4d5aac66e565fb4559f7d3b5
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2021-04-17 12:05:03 +0800

    Use latest view names

--- meistery_trial_assignment/api/urls.py
+++ meistery_trial_assignment/api/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.urls import include, path
 
-from .users.views import CustomAuthToken, LogoutView
+from .users.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("login", CustomAuthToken.as_view(), name="login"),
+    path("login", LoginView.as_view(), name="login"),
     path("logout", LogoutView.as_view(), name="logout"),
     path("", include("meistery_trial_assignment.api.users.urls")),
     path("", include("meistery_trial_assignment.api.sales.urls")),
