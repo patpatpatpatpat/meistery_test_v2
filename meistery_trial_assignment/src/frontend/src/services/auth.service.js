91c767c6a4521317ce60d5827a93e9652c0474e4
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2021-04-17 08:55:06 +0800

    Structure changes for herkou deployment

--- meistery_trial_assignment/src/frontend/src/services/auth.service.js
+++
@@ -1,9 +0,0 @@
-import axios from "../api/axios.instance";
-
-export const loginService = (data) => {
-  return axios.post("/api/v1/login", data);
-};
-
-export const logoutService = () => {
-  return axios.get("/api/v1/logout");
-};
--- meistery_trial_assignment/src/frontend/src/services/country.service.js
+++
@@ -1,5 +0,0 @@
-import axios from "../api/axios.instance";
-
-export const getCountryCityListService = () => {
-    return axios.get("/api/v1/country_data");
-  };
